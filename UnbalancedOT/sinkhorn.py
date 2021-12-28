fix 2 bugs: (1) doesnt stop after patience threshold (wrong stopping condiition), and (2) exploding value in calculation of B (switch to sumlogexp)

UnbalancedOT/sinkhorn.py
@@ -1,13 +1,14 @@
 import numpy as np
+from scipy.special import logsumexp
 
 from uot import UOT, EntRegUOT
-from uot import calc_f, calc_B
+from uot import calc_f, calc_B, calc_logB
 from utils import norm_inf
 
 
 def sinkhorn_raw(p: EntRegUOT,
                  niters: int,
-                 float_type=np.float128):
+                 float_type=np.float64):
     # Find problem dimension
     n = p.C.shape[0]
 
@@ -32,11 +33,12 @@ def sinkhorn_raw(p: EntRegUOT,
 
 
 def sinkhorn(p: EntRegUOT,
-             niters: int,
+             k_stop: int,
              save_uv: bool = True,
-             float_type=np.float128,
+             float_type=np.float64,
              verbose: bool = False):
     log = dict()
+    log['f'] = []
     if save_uv:
         log['u'] = []
         log['v'] = []
@@ -47,90 +49,38 @@ def sinkhorn(p: EntRegUOT,
     # Initialize
     u = np.zeros(n, dtype=float_type)
     v = np.zeros(n, dtype=float_type)
-
-    # Loop
-    scale = p.eta * p.tau / (p.eta + p.tau)
-    for k in range(niters):
-        if save_uv:
-            log['u'].append(u)
-            log['v'].append(v)
-
-        X = calc_B(p, u, v)
-
-        if verbose and k % 100 == 0:
-            print(k, calc_f(p, X))
-
-        # Update
-        if k % 2 == 0:
-            ak = X.sum(-1)
-            u = (u / p.eta + np.log(p.a / ak)) * scale
-        else:
-            bk = X.sum(0)
-            v = (v / p.eta + np.log(p.b / bk)) * scale
 
     if save_uv:
         log['u'].append(u)
         log['v'].append(v)
 
-    if save_uv:
-        log['u'] = np.vstack(log['u'])
-        log['v'] = np.vstack(log['u'])
-
-    return calc_B(p, u, v), log
-
-
-def sinkhorn_eps(p: EntRegUOT,
-                 f_optimal: float,
-                 eps: float,
-                 patience: int = 0,
-                 save_uv: bool = True,
-                 float_type=np.float128,
-                 verbose: bool = False):
-    log = dict()
-    if save_uv:
-        log['u'] = []
-        log['v'] = []
-    log['f'] = []
-
-    # Find problem dimension
-    n = p.C.shape[0]
-
-    # Initialize
-    u = np.zeros(n, dtype=float_type)
-    v = np.zeros(n, dtype=float_type)
-
     # Loop
     scale = p.eta * p.tau / (p.eta + p.tau)
 
     k = 0
-    c = 0
     while True:
-        if save_uv:
-            log['u'].append(u)
-            log['v'].append(v)
+        Xk = calc_logB(p, u, v)
 
-        X = calc_B(p, u, v)
-        f = calc_f(p, X)
-
-        if verbose and k % 100 == 0:
-            print(k, f)
-
+        f = calc_f(p, np.exp(Xk))
         log['f'].append(f)
 
-        if f - f_optimal <= eps:
-            c += 1
-            if c > patience:
-                break
-            else:
-                c = 1
+        if verbose and k % 1000 == 0:
+            print(k, f)
+
+        if k >= k_stop:
+            break
 
         # Update
         if k % 2 == 0:
-            ak = X.sum(-1)
-            u = (u / p.eta + np.log(p.a / ak)) * scale
+            log_ak = logsumexp(Xk, -1)
+            u = (u / p.eta + np.log(p.a) - log_ak) * scale
         else:
-            bk = X.sum(0)
-            v = (v / p.eta + np.log(p.b / bk)) * scale
+            log_bk = logsumexp(Xk, 0)
+            v = (v / p.eta + np.log(p.b) - log_bk) * scale
+
+        if save_uv:
+            log['u'].append(u)
+            log['v'].append(v)
 
         k += 1
 
@@ -138,8 +88,73 @@ def sinkhorn_eps(p: EntRegUOT,
         log['u'] = np.vstack(log['u'])
         log['v'] = np.vstack(log['v'])
 
-    return calc_B(p, u, v), log
+    return Xk, log
 
+
+def sinkhorn_eps(p: EntRegUOT,
+                 f_optimal: float,
+                 eps: float,
+                 patience: int = 0,
+                 save_uv: bool = True,
+                 float_type=np.float64,
+                 verbose: bool = False):
+    log = dict()
+    log['f'] = []
+    if save_uv:
+        log['u'] = []
+        log['v'] = []
+
+    # Find problem dimension
+    n = p.C.shape[0]
+
+    # Initialize
+    u = np.zeros(n, dtype=float_type)
+    v = np.zeros(n, dtype=float_type)
+
+    if save_uv:
+        log['u'].append(u)
+        log['v'].append(v)
+
+    # Loop
+    scale = p.eta * p.tau / (p.eta + p.tau)
+
+    k = 0
+    c = 0
+    while True:
+        Xk = calc_logB(p, u, v)
+
+        f = calc_f(p, np.exp(Xk))
+        log['f'].append(f)
+
+        if verbose and k % 1000 == 0:
+            print(k, f)
+
+        if f - f_optimal <= eps:
+            c += 1
+            if c > patience:
+                break
+        else:
+            c = 1
+
+        # Update
+        if k % 2 == 0:
+            log_ak = logsumexp(Xk, -1)
+            u = (u / p.eta + np.log(p.a) - log_ak) * scale
+        else:
+            log_bk = logsumexp(Xk, 0)
+            v = (v / p.eta + np.log(p.b) - log_bk) * scale
+
+        if save_uv:
+            log['u'].append(u)
+            log['v'].append(v)
+
+        k += 1
+
+    if save_uv:
+        log['u'] = np.vstack(log['u'])
+        log['v'] = np.vstack(log['v'])
+
+    return Xk, log
 
 # =========================================================
 
@@ -170,18 +185,3 @@ def calc_k_stop(p: EntRegUOT, eps: float) -> float:
     k_float = (p.tau * U / eps + 1) * (np.log(8 * p.eta * R) +
                                        np.log(p.tau * (p.tau + 1)) + 3 * np.log(U / eps))
     return k_float
-
-
-def sinkhorn_entreg_uot(p: EntRegUOT,
-                        eps: float) -> np.ndarray:
-    # Find stopping condition
-    k_stop = calc_k_stop(p, eps)
-    niters = 1 + int(k_stop)
-
-    # Perform Sinkhorn iterations
-    X, log = sinkhorn(p, niters)
-
-    # Add k_stop to logger
-    log['k_stop'] = k_stop
-
-    return X, log

UnbalancedOT/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial.distance import cdist
+from scipy.special import xlogy
 
 
 def norm_inf(x: np.ndarray) -> float:
@@ -7,11 +8,11 @@ def norm_inf(x: np.ndarray) -> float:
 
 
 def calc_entropy(P: np.ndarray) -> float:
-    return -np.sum(P * np.log(P) - P)
+    return -np.sum(xlogy(P, P) - P)
 
 
 def calc_KL(x: np.ndarray, y: np.ndarray) -> float:
-    return np.sum(x * np.log(x / y) - x + y)
+    return np.sum(xlogy(x, x / y) - x + y)
 
 
 def get_distance_matrix(n: int, d: str = 'cityblock'):