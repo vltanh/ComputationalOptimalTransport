b96a3a1aa18ee277fadeabb220e98428de223c35
vltanh <narubees@gmail.com>
2021-12-29 11:40:45 +0700

    minor changes to RSOT

--- RobustOT/rsot.py
+++ RobustOT/rsot.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass
 
 import numpy as np
 import cvxpy as cp
-from scipy.special import logsumexp
 
 from utils import calc_KL, calc_entropy
 
@@ -77,7 +76,7 @@ def calc_h_rsot(p: EntropicRSOT,
         - (v @ p.b)
 
 
-def exact_entreg_rsot(p: EntropicRSOT):
+def exact_entreg_rsot(p: EntropicRSOT, verbose: bool = False):
     n = p.C.shape[0]
 
     u = cp.Variable(shape=n)
@@ -85,10 +84,29 @@ def exact_entreg_rsot(p: EntropicRSOT):
 
     h = p.eta * cp.sum(cp.exp((u[:, None] + v[None, :] - p.C) / p.eta)) \
         + p.tau * cp.sum(cp.multiply(cp.exp(-u / p.tau), p.a)) \
-        + cp.sum(cp.multiply(v, p.b))
+        - cp.sum(cp.multiply(v, p.b))
     obj = cp.Minimize(h)
 
     prob = cp.Problem(obj)
-    prob.solve()
+    prob.solve(verbose=verbose)
 
     return prob.value, u.value, v.value
+
+
+def exact_entreg_rsot_primal(p: EntropicRSOT, verbose: bool = False):
+    n = p.C.shape[0]
+    X = cp.Variable((n, n), nonneg=True)
+
+    f = cp.sum(cp.multiply(p.C, X)) \
+        + p.tau * cp.sum(cp.kl_div(cp.sum(X, 1), p.a)) \
+        - p.eta * cp.sum(cp.entr(X))
+    objective = cp.Minimize(f)
+
+    constraints = [
+        cp.sum(X, 0) == p.b,
+    ]
+
+    prob = cp.Problem(objective, constraints)
+    prob.solve(verbose=verbose)
+
+    return prob.value, X.value
