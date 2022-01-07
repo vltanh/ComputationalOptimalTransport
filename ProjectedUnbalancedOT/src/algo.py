fix some bugs (wrong update formula) and make some changes

ProjectedUnbalancedOT/src/algo.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.special import logsumexp
 
+from src.uot import UOT, EntropicUOT
 from src.puot import EntropicPUOT
 from src.utils import norm_inf
 
@@ -19,13 +20,67 @@ def round_plan(pi, a, b):
     return pi__ + err_r[:, None] @ err_c[None, :] / np.sum(np.abs(err_r))
 
 
-def solve(p: EntropicPUOT,
-          u0: np.ndarray, v0: np.ndarray,
-          U0: np.ndarray,
-          delta: np.float64,
-          eps_1: np.float64, eps_2: np.float64,
-          save_uv: bool = False,
-          save_U: bool = False):
+def solve_entropic_uot(p: EntropicUOT,
+                       u0: np.ndarray,
+                       v0: np.ndarray,
+                       niters: int):
+    # Initialize
+    u = u0.copy()
+    v = v0.copy()
+
+    # Loop
+    for k in range(niters):
+        log_pi = p.calc_logpi(u, v)
+
+        # Update
+        if k % 2 == 0:
+            log_ak = logsumexp(log_pi, -1)
+            u = (u / p.eta + np.log(p.a) - log_ak) \
+                * p.eta * p.tau[0] / (p.eta + p.tau[0])
+        else:
+            log_bk = logsumexp(log_pi, 0)
+            v = (v / p.eta + np.log(p.b) - log_bk) \
+                * p.eta * p.tau[1] / (p.eta + p.tau[1])
+
+    return u.copy(), v.copy()
+
+
+def calc_R(p: EntropicUOT) -> float:
+    n = p.C.shape[0]
+    R = max(norm_inf(np.log(p.a)), norm_inf(np.log(p.b))) + \
+        max(np.log(n), norm_inf(p.C) / p.eta - np.log(n))
+    return R
+
+
+def calc_U(p: UOT, eps: float) -> float:
+    n = p.C.shape[0]
+    alpha, beta = p.a.sum(), p.b.sum()
+
+    S = 0.5 * (alpha + beta) + 0.5 + 0.25 / np.log(n)
+    T = 0.5 * (alpha + beta) * (np.log(0.5 * (alpha + beta)) +
+                                2 * np.log(n) - 1) + np.log(n) + 2.5
+    U = max(S + T, 2 * eps, 4 * eps * np.log(n) / p.tau[0],
+            4 * eps * (alpha + beta) * np.log(n) / p.tau[0])
+
+    return U
+
+
+def calc_k_stop(p: EntropicUOT, eps: float) -> float:
+    R = calc_R(p)
+    U = calc_U(p, eps)
+    k_float = (p.tau[0] * U / eps + 1) * (np.log(8 * p.eta * R) +
+                                          np.log(p.tau[0] * (p.tau[0] + 1)) + 3 * np.log(U / eps))
+    return 1 + int(k_float)
+
+
+def solve_entropic_puot(p: EntropicPUOT,
+                        u0: np.ndarray, v0: np.ndarray,
+                        U0: np.ndarray,
+                        delta: np.float64,
+                        eps_uv: np.float64, eps_U: np.float64,
+                        max_uv: int = None, max_U: int = None,
+                        save_uv: bool = False,
+                        save_U: bool = False):
     u, v = u0.copy(), v0.copy()
     U = U0.copy()
 
@@ -37,31 +92,34 @@ def solve(p: EntropicPUOT,
     if save_U:
         log['U'] = []
 
-    rawC_inf = norm_inf(p.C)
-
     k = 0
     while True:
+        # === Update u/v ===
+
         # Compute projected cost matrix
         C = p.calc_proj_cost(U)
 
-        # Update u/v
-        for _ in range(1):
-            # Compute pi using current U and previous u, v
-            log_pi = p.calc_logpi(u, v, C)
+        # UOT problem
+        uot = UOT(C, p.a, p.b, p.tau)
 
-            # Update u
-            log_ak = logsumexp(log_pi, -1)
-            u = u + np.log(p.a) - log_ak
+        # Entropic UOT problem
+        eta = eps_uv / calc_U(uot, eps_uv)
+        euot = uot.entropic_regularize(eta)
 
-            # Compute pi using current U, u and previous v
-            log_pi = p.calc_logpi(u, v, C)
+        # Calculate k
+        n_uv = calc_k_stop(euot, eps_uv)
+        if max_uv is not None:
+            n_uv = min(n_uv, max_uv)
 
-            # Update v
-            log_bk = logsumexp(log_pi, 0)
-            v = v + np.log(p.b) - log_bk
+        # Solve Entropic UOT
+        u, v = solve_entropic_uot(euot, u, v, n_uv)
 
-        # Update U
-        for _ in range(1):
+        # === Update U ===
+        n_U = 0
+        while True:
+            # Compute projected cost matrix
+            C = p.calc_proj_cost(U)
+
             # Compute pi using current U, u, v
             pi = p.calc_pi(u, v, C)
 
@@ -79,8 +137,21 @@ def solve(p: EntropicPUOT,
             # Update U
             U, _ = np.linalg.qr(U - delta * xi)
 
+            n_U += 1
+
+            grad_norm = np.linalg.norm(xi)
+            if grad_norm <= eps_U or (max_U is not None and n_U >= max_U):
+                break
+
         # Log
+        C = p.calc_proj_cost(U)
+        pi = p.calc_pi(u, v, C)
+        A = p.X.T @ pi @ p.Y
+        Vpi = p.X.T @ np.diag(pi.sum(-1)) @ p.X  \
+            + p.Y.T @ np.diag(pi.sum(0)) @ p.Y \
+            - A - A.T
         f = np.trace(U.T @ Vpi @ U)
+
         log['f'].append(f)
         if save_uv:
             log['u'].append(u.copy())
@@ -89,13 +160,11 @@ def solve(p: EntropicPUOT,
             log['U'].append(U.copy())
 
         k += 1
-        if k % 200 == 0:
-            print(k, f)
+        if k % 1 == 0:
+            print(k, f, n_uv, n_U, grad_norm)
 
         # Check stopping condition
-        if 4 * p.eta * np.linalg.norm(xi) <= eps_1 \
-                and 8 * rawC_inf * np.linalg.norm(p.a - pi.sum(-1)) <= eps_2 \
-                and 8 * rawC_inf * np.linalg.norm(p.b - pi.sum(0)) <= eps_2:
+        if grad_norm <= eps_U and n_U == 1:
             break
 
     log['f'] = np.array(log['f'])

ProjectedUnbalancedOT/visualize.py
@@ -38,7 +38,7 @@ def visualize_3D_plan(X, Y, U, pi):
                 y=[X[i, 1], Y[j, 1]],
                 z=[X[i, 2], Y[j, 2]],
                 line=dict(
-                    width=pi[i, j] * 50,
+                    width=pi[i, j] * 10,
                     color='black',
                 ),
                 marker=dict(
@@ -49,12 +49,13 @@ def visualize_3D_plan(X, Y, U, pi):
                 showlegend=False
             ))
 
-    xp, yp, zp = get_plane(np.zeros(3), null_space(U.T).reshape(-1))
-    data += [
-        go.Surface(x=xp, y=yp, z=zp,
-                   opacity=0.1,
-                   showscale=False)
-    ]
+    if U is not None:
+        xp, yp, zp = get_plane(np.zeros(3), null_space(U.T).reshape(-1))
+        data += [
+            go.Surface(x=xp, y=yp, z=zp,
+                       opacity=0.1,
+                       showscale=False)
+        ]
 
     layout = go.Layout(
         margin={'l': 0, 'r': 0, 'b': 0, 't': 0}